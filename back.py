d527fbb7da26780ccc8827defefd511f97f09b7f
dami0 <sracz0@gmail.com>
2013-08-07 16:52:14 +0100

    Redid sysargs

--- back.py
+++ back.py
@@ -5,10 +5,12 @@
 # personalised to my [or your] system to allow for easy customisation of the
 # output from the backup program itself [currently rdiff-backup].
 
+# GPLv3, read LICENSE in ./
+
 import sys
 import subprocess
 
-def conf(pac): #create config file for the shell scripts
+def conf(pac):   #create config file for the shell scripts
   conff = open('./conf.dat', 'w')
   conff.write('#!/usr/bin/bash\n\n')
   conff.write(pac+'\n\n')
@@ -16,31 +18,39 @@ def conf(pac): #create config file for the shell scripts
   conff.write('mkdir $DIR\nfi')
   conff.close()
 
-def pacman():
+def pacman():    #pacman stuff
   subprocess.call('./pacbac.sh')
   print 'Exported list of packages for pacman.'
 
-def rdiff():
+def rdiff():     #rdiff-backup stuff
   subprocess.call('./rdbac.sh')
   print 'Rdiff-backup has performed its thing.'
 
-def main(): #probably want to put configuration and state in here
+def printhelp()
+  print 'Not implemented yet.'
+  exit
+  
+def main():      #main program
+  if len(sys.argv) > 1:
+    ne = 1
+    if 'h' in sys.argv[1]:
+      printhelp()
+    elif not 'd' in arg and not 'f' in arg and not 'p' in arg:
+      print 'Unknown option(s):' + sys.argv[1]
+      printhelp()
+
   path = 'DIR=/home/d/.backup/'
-  if len(sys.argv) >= 2:
-    if 'd' in sys.argv[1]:
-      path = 'DIR='+raw_input('DIR=')
-  if len(sys.argv) >= 3:
-    open('', 'rU')
+  if ne and 'd' in sys.argv[1]:
+    path = 'DIR='+raw_input('DIR=')
 
   conf(path)
 
-  if len(sys.argv) >= 2:
+  if ne:
     if 'p' in sys.argv[1]:
       pacman()
-
-  if len(sys.argv) >= 2:
-    if 'f' in sys.argv[1]:
+    if 'p' in sys.argv[1]:
       rdiff()
 
 if __name__ == '__main__': # boilerplate run if called as program
   main()
+
