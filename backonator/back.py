64737deddf0e907adaca2c9e543312203e1a0d57
dami0 <sracz0@gmail.com>
2013-08-10 17:08:00 +0100

    Commented

--- backonator/back.py
+++ backonator/back.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python2
 
+# Code by Damian A. Wloch, github.com/dami0, sracz0@gmail.com
+
 # This is a personal script, quite unfinished to help me secure my computer
 # state and personal files by way of automated/scripted backup solutions
 # personalised to my [or your] system to allow for easy customisation of the
@@ -11,41 +13,40 @@ import sys
 import subprocess
 import re
 
-def pacman(drt): #pacman stuff
+def pacman(drt): #pacman script with directory passed from main
   subprocess.call(['./scripts/pacbac.sh', drt])
   print 'Exported list of packages for pacman.'
 
-def rdiff(drt):  #rdiff-backup stuff
+def rdiff(drt):  #rdiff-backup script with directory passed from main
   subprocess.call(['./scripts/rdbac.sh', drt])
   print 'Rdiff-backup has performed its thing.'
 
-def printhelp():
+def printhelp(): #display help text and exit afterwards
   helpme = open('./help.me', 'rU')
   sys.stdout.write(helpme.read())
   helpme.close()
   exit()
 
 def main():      #main program
-  ne = 0
-  if len(sys.argv) > 1:
-    ne = 1
-    match = re.findall('[^-dfphn]', sys.argv[1])
-    if match:
-      print 'Unknown option(s): ' + str(match) + '\n'
-      printhelp()
-    if 'h' in sys.argv[1]: printhelp()
+  ne = 0 #assign sys.argv flag
+  if len(sys.argv) > 1: #part of code responsible for help and unknowns
+    ne = 1 #sys.argv flag on
+    if re.findall('[^-dfphn]', sys.argv[1]): #code for unknowns, if ANY unknown
+      print 'Unknown option(s): ' + str(match) + '\n' 
+      printhelp() #display correct options as well
+    if 'h' in sys.argv[1]: printhelp() #help option check
 
-  path = '/home/d/.backup/'
+  path = '/home/d/.backup/' #standard dir, can be modified by user
   if ne and 'd' in sys.argv[1]:
-    path = raw_input('DIR=')
+    path = raw_input('DIR=') #user input for non-standard dir
 
-  if ne and 'p' in sys.argv[1]:
+  if ne and 'p' in sys.argv[1]: #running appropriate stuff at appropriate times
     pacman(path)
   if ne and 'f' in sys.argv[1]:
     rdiff(path)
 
-  if ne: dty = 'Backonator has run with ' + sys.argv[1] + ' flags.'
-  else: dty = 'Backonator has reconfigured'
+  if ne: dty = 'Backonator has run with ' + sys.argv[1] + ' flags.' #wmii pager
+  else: dty = 'Backonator has reconfigured.'                        #code
   subprocess.call(['./scripts/msg.sh', dty + '::3'])
 
 if __name__ == '__main__': # boilerplate run if called as program
--- pager/page.py
+++ pager/page.py
@@ -3,20 +3,20 @@
 import subprocess
 
 def main():
-  newlist = []
-  msg_list = open('/home/d/.msg/notelist', 'rU')
+  newlist = [] #list for rewriting the file
+  msg_list = open('/home/d/.msg/notelist', 'rU') #open list of stuff to display
   for line in msg_list.readlines():
-    curr = line.split('::')
-    if len(curr) > 1:
-      if '::k' not in line: curr[1] = int(curr[1].strip('\n'))
-      if '::k' not in line: curr[1] -= 1
-      subprocess.call(['/home/d/python/ahn/pager/wmiir.sh', curr[0]])
-      newlist.append(curr[0] + '::' + str(curr[1]) + '\n')
+    curr = line.split('::') #seperate message from number of time to display
+    if len(curr) > 1: #check to make sure message is formatted properly
+      if '::k' not in line: curr[1] = int(curr[1].strip('\n')) #extract the
+      if '::k' not in line: curr[1] -= 1                       #display time
+      subprocess.call(['/home/d/python/ahn/pager/wmiir.sh', curr[0]]) #script
+      newlist.append(curr[0] + '::' + str(curr[1]) + '\n') #Generate message -1
   msg_list.close()
 
-  msg_list = open('/home/d/.msg/notelist', 'w')
+  msg_list = open('/home/d/.msg/notelist', 'w') #regenerate the notelist
   for line in newlist:
-    if '::0' not in line:
+    if '::0' not in line: #drop after run through x times
       msg_list.write(line)
   msg_list.close()
 
