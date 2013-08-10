11a73f69e0cb0b1bb8ea1f13c2cd3009fb6cb968
dami0 <sracz0@gmail.com>
2013-08-10 16:39:16 +0100

    Cleaned up and improved

--- pager/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-MSG=TEST
-
-wmiir create /rbar/note &
-
-echo $MSG | wmiir write /rbar/note
--- pager/wmiir.sh
+++ pager/wmiir.sh
@@ -2,17 +2,12 @@
 
 export DISPLAY=:0
 
-wmiir create /rbar/note &
-echo $1 | wmiir write /rbar/note
-sleep 1
-wmiir remove /rbar/note
-sleep 1
-wmiir create /rbar/note &
-echo $1 | wmiir write /rbar/note
-sleep 1
-wmiir remove /rbar/note
-sleep 1  
-wmiir create /rbar/note &
-echo $1 | wmiir write /rbar/note
-sleep 1
-wmiir remove /rbar/note
+COUNTER=0
+while [  $COUNTER -lt 5 ]; do
+  wmiir create /rbar/note &
+  echo $1 | wmiir write /rbar/note
+  sleep 1
+  wmiir remove /rbar/note
+  sleep 1
+  let COUNTER=COUNTER+1 
+done
